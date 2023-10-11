23431d1b8e3e20aa5d9f1ff7ba19cf6b471c70e6
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-11 15:26:48 +0200

    First Commit

---
+++ src/esercizio3di1.java
@@ -0,0 +1,11 @@
+public class esercizio3di1 {
+    public static int somma(int a, int b){
+        return a + b;
+    };
+    public static void main(String[] args) {
+        
+        int risultato = somma(9, 9);
+        System.out.println("risultato = " + risultato);
+
+    }
+}
